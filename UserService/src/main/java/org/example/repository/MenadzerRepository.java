b44ec68eb158b12234b9a6ad3826d76ee7ce4368
Your Name <amaric2519rn@raf.rs>
2024-01-16 13:48:27 +0100

    Register fix

---
+++ UserService/src/main/java/org/example/repository/MenadzerRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+import org.example.domain.Menadzer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenadzerRepository extends JpaRepository<Menadzer, Integer> {
+}
